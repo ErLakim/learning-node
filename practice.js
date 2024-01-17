f324589a7fddba2954e4ba45d0a8e056b247f5f8
lakim dahal <lakimdahal1@gmail.com>
2024-01-17 09:27:36 +0545

    practicing functions in node

---
+++ practice.js
@@ -0,0 +1,10 @@
+//write a js function to add 2 number  and print
+let sum = (a, b) => 
+  a + b;
+console.log(sum(2,3));
+
+
+// write a js function to that checks username and password.
+// use bootstrap for login page design
+// result alert you have logged in
+// set username and password as your Email
