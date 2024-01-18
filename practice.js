e62f578466143bf6b93303edf0499475dedc16a5
lakim dahal <lakimdahal1@gmail.com>
2024-01-18 09:05:35 +0545

    running local host for first time

--- practice.js
+++ practice.js
@@ -1,10 +1,18 @@
 //write a js function to add 2 number  and print
-let sum = (a, b) => 
-  a + b;
-console.log(sum(2,3));
-
+// let sum = (a, b) =>
+//   a + b; implicit function
+// console.log(sum(2,3));
 
 // write a js function to that checks username and password.
 // use bootstrap for login page design
 // result alert you have logged in
-// set username and password as your Email
+// set username and password as your Email till friday.
+
+//using http funcion
+const http = require("http"); //object creation
+http
+  .createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/html" });
+  })
+  .listen(8000);
+console.log("Server Running at port 8000");
